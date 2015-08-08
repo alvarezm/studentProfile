cb48f20f08a7e190c8d68f2d514ecee5f0f051dd
Murielle Alvarez <alvarezmurielle@gmail.com>
2015-08-08 16:16:28 +0800

    initial setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name studentProfileApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the studentProfileApp
+ */
+angular.module('studentProfileApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name studentProfileApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the studentProfileApp
+ */
+angular.module('studentProfileApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
